03db70564a9814bb76e4c539c7c8820e442b3f71
Michael Ruoss <michael.ruoss@ufirstgroup.com>
2016-10-18 14:40:35 +0200

    add mandatory group id

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/ash
 
 if [ -z "$USER_ID" ]; then
-  echo "USER_ID is required"
+  echo "USER_ID is required - you may use \$(id -u)"
+  exit 1
+fi
+
+if [ -z "$GROUP_ID" ]; then
+  echo "GROUP_ID is required - you may use \$(id -g)"
   exit 1
 fi
 
 # something resets HOME => setting it again
-exec su-exec $USER_ID env HOME=$HOMEDIR "$@"
+exec su-exec $USER_ID:$GROUP_ID env HOME=$HOMEDIR "$@"
